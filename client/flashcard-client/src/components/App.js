602077670bf3eff9a6f5b00fd3daededd7194ff0
Peter Jeon <peterjeon2@gmail.edu>
2021-04-05 19:21:46 -0700

    set up redux

---
+++ client/flashcard-client/src/components/App.js
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+
+export default function App() {
+    const dispatch = useDispatch()
+}
---
+++ client/flashcard-client/src/middleware/index.js
@@ -0,0 +1,6 @@
+import thunk from 'redux-thunk'
+import { applyMiddleware, applyMiddleware } from 'redux'
+
+export default applyMiddleware(
+    thunk
+)
